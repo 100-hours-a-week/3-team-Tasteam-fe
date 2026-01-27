0dd0307e7ebc503cb31b39fd983142e7399bdbce
SH Woo <imdlsrks.mc@gmail.com>
2026-01-27 10:51:48 +0900

    ✨ feat: Add VITE_AUTH_DEBUG console log to main.tsx
    Logs the value of VITE_AUTH_DEBUG at application startup for debugging purposes.

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,9 @@ import { AppProviders } from '@/app/providers/AppProviders'
 import { BrowserRouter } from 'react-router-dom'
 import { HealthStatusIndicator } from '@/widgets/health-status/HealthStatusIndicator'
 import { AuthStatusIndicator } from '@/widgets/auth-status/AuthStatusIndicator'
+import { AUTH_DEBUG } from '@/shared/config/env' // Added import
+
+console.log('VITE_AUTH_DEBUG:', AUTH_DEBUG) // Added log statement
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
