de63176f2c6bbe6a8bf43820933ca1ff88f4884c
SH Woo <imdlsrks.mc@gmail.com>
2026-01-31 14:06:00 +0900

    🐛 fix: 상태 인디테이터 위치 및 토글 설정 추가

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,14 @@ import './shared/styles/global.css'
 import App from './App.tsx'
 import { AppProviders } from '@/app/providers/AppProviders'
 import { BrowserRouter } from 'react-router-dom'
-import { HealthStatusIndicator } from '@/widgets/health-status/HealthStatusIndicator'
-import { AuthStatusIndicator } from '@/widgets/auth-status/AuthStatusIndicator'
+import { DebugIndicators } from '@/widgets/debug/DebugIndicators'
 
 console.log('App starting. Checking console visibility.') // Modified log statement for testing
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <AppProviders>
-      <HealthStatusIndicator />
-      <AuthStatusIndicator />
+      <DebugIndicators />
       <App />
     </AppProviders>
   </BrowserRouter>,
